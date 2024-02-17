eee44fd9a9a1c6da361abbb8bfff83cdbb7c560b
calilisantos <calilisantos@gmail.com>
2024-02-17 12:16:37 -0300

    feat(dim_model): add read configs

---
+++ dim_model/configs/read.py
@@ -0,0 +1,17 @@
+FIRST_YEAR: int = 2016
+IGNORE_INDEX: bool = True
+LAST_YEAR: int = 2023
+MONTHS_LIST: list = [
+    "JANEIRO",
+    "FEVEREIRO",
+    "MARÇO",
+    "ABRIL",
+    "MAIO",
+    "JUNHO",
+    "JULHO",
+    "AGOSTO",
+    "SETEMBRO",
+    "OUTUBRO",
+    "NOVEMBRO",
+    "DEZEMBRO"
+]
