1daec0e1a3bf79eb8ee7adcb58201254b521297e
calilisantos <calilisantos@gmail.com>
2024-02-17 12:18:09 -0300

    feat(dim_model): add raw layer configs

---
+++ dim_model/configs/raw_layer.py
@@ -0,0 +1,14 @@
+RAW_COLUMNS_LIST: list = [
+    "CLIENTE",
+    "BANCO",
+    "Nº CHEQUE",
+    "VALOR (R$)",
+    "VENCIMENTO",
+    "SAÍDA",
+    "COD",
+    "DESTINO",
+    "OBSERVAÇÃO"
+]
+RAW_TABLE_NAME: str = "raw_data"
+REQUIRED_COLUMNS: int = 5  # CLIENTE, BANCO, Nº CHEQUE, VALOR (R$), VENCIMENTO
+SET_INDEX: bool = True
